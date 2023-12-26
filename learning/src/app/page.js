2c9ac65a229d3e3a5ddf909ae9c7bf090fb2359d
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-12-26 16:14:25 +0530

    routeing

--- learning/src/app/page.js
+++ learning/src/app/page.js
@@ -1,15 +1,30 @@
 'use client'
+
+import { useState } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+  const [count, setCount] = useState(0)
 
-  const printFruit = (fruit)=>{
-    alert(fruit)
+  const handleCount = ()=>{
+    setCount(count+1)
+  }
+
+  const InnerComponenet = (props) =>{
+    return <main>
+      <h1>InnerComponenet</h1>
+      {props.count}
+    </main>
   }
   return (
     <main className={styles.main}>
-      <h1>Home Page</h1>
-      <button onClick={()=>printFruit("mango")}>Click me</button>
+
+      <button onClick={()=>alert("hello")}>show alert</button>
+
+      <button onClick={handleCount}>Counter:- {count}</button>
+
+      <InnerComponenet count={count}/>
+
     </main>
   )
 }
