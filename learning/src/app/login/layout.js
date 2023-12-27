2cff8e898fe7985ffc0ad99af92e092765637716
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-12-27 11:55:57 +0530

    condetional layout

--- learning/src/app/login/layout.js
+++ learning/src/app/login/layout.js
@@ -3,21 +3,25 @@
 import React from 'react';
 import "./loginstyle.css"
 import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 function Layout({children}) {
 const router = useRouter()
+const path = usePathname()
 
 const routerFunction = (name) => {
     router.push(name);
 }
     return (
         <div>
-            <ul className='navbar-list'>
+            {
+                path !== "/login/loginguard" ? <ul className='navbar-list'>
                 <li onClick={()=>routerFunction("/login/loginstudent")}>Student</li>
                 <li onClick={()=>routerFunction("/login/loginteacher")}>Teacher</li>
                 <li onClick={()=>routerFunction("/login/loginwarden")}>Warden</li>
                 <li onClick={()=>routerFunction("/login/loginguard")}>Guard</li>
-            </ul>
+            </ul> : null
+            }
             {children}
         </div>
     );
